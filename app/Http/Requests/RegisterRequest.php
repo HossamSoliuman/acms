3fdfbd24ea4abd40abbaf1cf671e58520ad0552e
Hossam <hossamsoliuman@gmail.com>
2024-05-30 13:44:06 +0300

    set is_eng to be boolean

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users|max:255',
             'password' => 'required|string|min:8',
-            'is_eng' => 'nullable'
+            'is_eng' => 'nullable|boolean'
         ];
     }
 }
